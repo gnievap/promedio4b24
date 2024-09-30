545e234846040b176c80bad691c6d860318faf38
Gaby Nieva <gaby.nieva@outlook.com>
2024-09-30 13:47:46 -0600

    Creación de la clase Estudiante

---
+++ Estudiante.java
@@ -0,0 +1,74 @@
+public class Estudiante {
+    private String matricula;
+    private String nombre;
+    private double estructuras;
+    private double evaluacion;
+    private double ingles;
+    private double promedio;
+
+    // constructor
+    public Estudiante(String m, String n){
+        // El objetivo es construir e inicializar el objeto
+        this.matricula = m;
+        this.nombre = n;
+    }
+    
+    // Getters y Setters
+
+    // Get: método consultor
+    public String getMatricula() {
+        return matricula;
+    }
+
+    // Set: método modificador
+    public void setMatricula(String matric) {
+        this.matricula = matric;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public double getEstructuras() {
+        return estructuras;
+    }
+
+    public void setEstructuras(double estructuras) {
+        this.estructuras = estructuras;
+    }
+
+    public double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public double getIngles() {
+        return ingles;
+    }
+
+    public void setIngles(double ingles) {
+        this.ingles = ingles;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante [matricula=" + matricula + ", nombre=" + nombre + ", promedio=" + promedio + "]";
+    }
+
+    
+}
